cdfc89f44707edf846d1b14768c542e5a50aebf2
Stanislav Mekhonoshin <Stanislav.Mekhonoshin@dataart.com>
2016-12-08 15:47:23 +0300

    Spawn bees after honeycomb initialization

--- lib/ex_bees/actor/honeycomb.ex
+++ lib/ex_bees/actor/honeycomb.ex
@@ -1,38 +1,46 @@
 defmodule ExBees.Honeycomb do
   use GenServer
 
-  defstruct name: nil, bees: [], honey: 0, position: {0, 0}
+  defstruct name: nil, bees: 0, honey: 0, position: {0, 0}
 
   def start_link(name) do
     GenServer.start_link(__MODULE__, name, name: name)
   end
 
+  def spawn_bee(pid) do
+    GenServer.cast(pid, :spawn_bee)
+  end
+
   # Callbacks
 
   def init(name) do
-    # TODO: Use name instead of pid
     position = ExBees.Map.allocate_honeycomb(self())
     state = %ExBees.Honeycomb{name: name, position: position}
 
-    # TODO: emit bees after start, on timer event
-    bees_number = Application.get_env(:ex_bees, :bees_per_honeycomb)
-    initial_bees = for i <- 1..bees_number do
-      spawn_bee(i, state)
-    end
-
-    {:ok, %{state | bees: initial_bees}}
+    spawn_bees
+    {:ok, state}
   end
 
-  defp spawn_bee(index, state) do
-    # TODO: atoms are not GCed
+  def handle_cast(:spawn_bee, state) do
+    index = next_bee_index(state)
+
+    # TODO: monitor bees
     "Bee.#{state.name}.#{index}"
     |> String.to_atom
     |> ExBees.Bee.start_link(state.position)
+
+    {:noreply, %{state | bees: index}}
   end
 
-  #defp tick() do
-    #period = Application.get_env(:ex_bees, :tick_period)
-    #Process.send_after(self(), :tick, period)
-  #end
+  defp spawn_bees do
+    bees_number = Application.get_env(:ex_bees, :bees_per_honeycomb)
 
+    for index <- 1..bees_number do
+      ExBees.Honeycomb.spawn_bee(self())
+    end
+  end
+
+  defp next_bee_index(state) do
+    state.bees + 1
+  end
 end
